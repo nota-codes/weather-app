b929b3b8d33288542c0ac620f8b76fe1ed82e27c
jonathanwirz <jonathan.wirz@icloud.com>
2024-09-14 18:10:30 +0200

    add location feature

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const apiUrl =
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
 const weatherIcon = document.querySelector(".weather-icon");
+const searchLocation = document.querySelector(".locationButton");
 
 async function checkWeather(city) {
   const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
@@ -36,8 +37,64 @@ async function checkWeather(city) {
     document.querySelector(".Weather").style.display = "block";
     document.querySelector(".error").style.display = "none";
   }
-}
+};
+
+async function checkWeatherByCoords(lat, lon) {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`)
+
+    if (response.status == 404) {
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".Weather").style.display = "none";
+      } else {
+        var data = await response.json();
+        console.log(data);
+    
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".temp").innerHTML =
+          Math.round(data.main.temp) + "°C";
+        document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+        document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+        document.querySelector(".search input").value = "Malters";
+    
+        if (data.weather[0].main == "Clouds") {
+          weatherIcon.src = "images/clouds.png";
+        } else if (data.weather[0].main == "Clear") {
+          weatherIcon.src = "images/clear.png";
+        } else if (data.weather[0].main == "Rain") {
+          weatherIcon.src = "images/rain.png";
+        } else if (data.weather[0].main == "Drizzle") {
+          weatherIcon.src = "images/drizzle.png";
+        } else if (data.weather[0].main == "Mist") {
+          weatherIcon.src = "images/mist.png";
+        }
+    
+        document.querySelector(".Weather").style.display = "block";
+        document.querySelector(".error").style.display = "none";
+      }
+
+};
+
+
 
 searchBtn.addEventListener("click", () => {
   checkWeather(searchBox.value);
 });
+
+searchLocation.addEventListener("click", () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const lat = position.coords.latitude;
+          const lon = position.coords.longitude;
+          console.log("Latitude is :", lat, "Longitude is :", lon);
+          checkWeatherByCoords(lat, lon);
+        },
+        (error) => {
+          console.error("Error getting location:", error);
+          alert("Unable to retrieve your location.");
+        }
+      );
+    } else {
+      alert("Geolocation is not supported by this browser.");
+    }
+  });
